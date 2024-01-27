2e2ce7e30a63f1af466fc1152bb596a1fe02bea6
mtalaga20 <michael.talaga@hotmail.com>
2024-01-27 12:02:55 -0500

    Update

--- data_analysis/correlation.py
+++ data_analysis/correlation.py
@@ -7,7 +7,7 @@ csv_path = r'C:\Users\mktal\repos\College_Basketball_Game_Prediction\CSV_Data\\'
 df = pd.read_csv(csv_path+f'2023\\2023_data.csv')
 df = df.iloc[:,2:]
 
-for year in [2012,2013,2014,2015,2016,2017,2018,2019,2021,2022]:
+for year in [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2021,2022]:
     small_df = pd.read_csv(csv_path+f'{year}\\{year}_data.csv')
     small_df = small_df.iloc[:,2:]
     df = pd.concat([df,small_df])
